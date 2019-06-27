e9e62b8a0177be3a267b9eb4b7a1af5c92f7f801
Gabriel Kothe <g.kothe@hotmail.com>
2019-06-27 11:07:45 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-(function($) {
-	"use strict"
+function init() {
 
 	///////////////////////////
 	// Preloader
-	$(window).on('load', function() {
+	$(window).on('load', function () {
 		$("#preloader").delay(600).fadeOut();
 	});
 
@@ -16,7 +15,7 @@
 
 	///////////////////////////
 	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+	$("#nav .main-nav a[href^='#']").on('click', function (e) {
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
@@ -24,7 +23,7 @@
 		}, 600);
 	});
 
-	$('#back-to-top').on('click', function(){
+	$('#back-to-top').on('click', function () {
 		$('body,html').animate({
 			scrollTop: 0
 		}, 600);
@@ -32,19 +31,19 @@
 
 	///////////////////////////
 	// Btn nav collapse
-	$('#nav .nav-collapse').on('click', function() {
+	$('#nav .nav-collapse').on('click', function () {
 		$('#nav').toggleClass('open');
 	});
 
 	///////////////////////////
 	// Mobile dropdown
-	$('.has-dropdown a').on('click', function() {
+	$('.has-dropdown a').on('click', function () {
 		$(this).parent().toggleClass('open-drop');
 	});
 
 	///////////////////////////
 	// On Scroll
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var wScroll = $(this).scrollTop();
 
 		// Fixed nav
@@ -64,106 +63,124 @@
 	///////////////////////////
 	// Owl Carousel
 	$('#tragoweb-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		smartSpeed:10,
-		dotsSpeed:10
+		smartSpeed: 10,
+		dotsSpeed: 10
 	});
 	$('#trago-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		
+
 	});
 
 	$('#menyu-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		
+
 	});
 	$('#xwing-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		
+
 	});
 
 	$('#casher-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		
+
 	});
 
 	$('#kafer-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut',
-		
+
 	});
 
 
 	$('#testimonial-slider').owlCarousel({
-		loop:true,
-		margin:15,
-		dots : true,
+		loop: true,
+		margin: 15,
+		dots: true,
 		nav: false,
-		autoplay : true,
-		responsive:{
+		autoplay: true,
+		responsive: {
 			0: {
-				items:1
+				items: 1
 			},
-			992:{
-				items:2
+			992: {
+				items: 2
 			}
 		}
 	});
 
-	teste();
-	
-})(jQuery);
 
 
-
-function teste(){
-	
-	var cont = 1;
-	console.log(cont++);
-	console.log(cont++);
-	console.log(cont++);
-	
-	
-	
 }
+
+var idioma = "P";
+$(document).ready(function () {
+	init();
+	changeLanguage();
+});
+
+
+function changeLanguage() {
+	
+	idioma = idioma == "P" ? "E" : "P";
+
+	if (idioma == "E") {
+		$(".brazil_i").hide();
+		$(".english_i").show();
+	} else if (idioma == "P") {
+		$(".brazil_i").show();
+		$(".english_i").hide();
+	} 
+}
+
+// function teste(){
+
+// 	var cont = 1;
+// 	console.log(cont++);
+// 	console.log(cont++);
+// 	console.log(cont++);
+
+
+
+// }
