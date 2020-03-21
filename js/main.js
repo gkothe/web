1bee8c90b4856a88ce48ae2d2703da981412d2a1
Gabriel Kothe <g.kothe@hotmail.com>
2020-03-21 17:27:11 -0300

    news

--- js/main.js
+++ js/main.js
@@ -133,6 +133,18 @@ function init() {
 
 	});
 
+	$('#delivery-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 15,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
+		animateOut: 'fadeOut',
+
+	});
+
 
 	$('#testimonial-slider').owlCarousel({
 		loop: true,
